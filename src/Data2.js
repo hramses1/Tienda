0d5427d4801f5ecf9c5aa7009689eda89cbc31eb
hramses1 <79066311+hramses1@users.noreply.github.com>
2021-02-21 17:11:26 -0500

    nice

---
+++ src/Data2.js
@@ -0,0 +1,117 @@
+const data = {
+  status: "success",
+  items: [
+    {
+      id: 1,
+      title: "Ahri",
+      price: 1022,
+      image: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg",
+      category: "Jonia",
+      img1: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606321430&w=1000",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      title: "Aatrox",
+      price: 1924,
+      image: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg",
+      category: "Darkin",
+      img1: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606319825&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 3,
+      title: "Alistar",
+      price: 2125,
+      image: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Alistar_0.jpg",
+      category: "Jonia",
+      img1: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Alistar_0.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325289&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 4,
+      title: "Amumu",
+      price: 2312,
+      image: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Amumu_0.jpg",
+      category: "Shurima",
+      img1: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Amumu_0.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325338&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 5,
+      title: "Dario",
+      price: 21,
+      image: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Darius_0.jpg",
+      category: "Noxus",
+      img1: " https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Darius_0.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606322330&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 6,
+      title: "Khada Jhin",
+      price: 4444,
+      image: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jhin_0.jpg",
+      category: "Jonia",
+      img1: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jhin_0.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606321824&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 7,
+      title: "Lucian",
+      price: 2223,
+      image: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Lucian_0.jpg",
+      category: "Piltover",
+      img1: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Lucian_0.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606320966&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 8,
+      title: "Mordekaiser",
+      price: 2242,
+      image: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Mordekaiser_0.jpg",
+      category: "Noxus",
+      img1: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Mordekaiser_0.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325265&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 9,
+      title: "Viego",
+      price: 2325,
+      image: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Viego_0.jpg",
+      category: "Islas Santas",
+      img1: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Viego_0.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606323250&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 10,
+      title: "Varus",
+      price: 2121,
+      image: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Varus_0.jpg",
+      category: "Darkin",
+      img1: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Varus_0.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1609445259&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 11,
+      title: "Thresh",
+      price: 2312,
+      image: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Thresh_0.jpg",
+      category: "Islas Santas",
+      img1: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Thresh_0.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1609438911&w=1000",
+      cantidad: 1
+    },
+  ],
+};
+
+export default data;
